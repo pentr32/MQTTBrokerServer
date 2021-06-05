d5bf32dbb47e2452aa4beba2a190350ee2572cce
Martin Sonne <Martin Sonne@LAPTOP-JULDTJEK>
2021-06-05 17:54:35 +0200

    Added DataLayer and EF core in progress...

---
+++ DataLayer/Entities/Measurement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Entities
+{
+    public class Measurement
+    {
+        public int Id { get; set; }
+        public float Temperature { get; set; }
+        public float Humidity { get; set; }
+        public DateTime TimeCreated { get; set; }
+    }
+}
---
+++ DataLayer/MeasurementsContext.cs
@@ -0,0 +1,25 @@
+﻿using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class MeasurementsContext : DbContext
+    {
+        private const string connectionString = "server=192.168.42.14;port=3306;database=mqttdb;user=mqttdbuser;password=P@ssw0rd";
+        //private const string connectionString = "server=192.168.42.14;port=3306;database=mqqtdb;uid=mqttdbuser@localhost;pwd=P@ssw0rd;";
+
+        public DbSet<Measurement> Measurements { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+
+    }
+}
